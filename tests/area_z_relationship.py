ae2555079855828277ee785ca9c08789888d6fd6
javmarina <javmarina@uma.es>
2021-02-06 11:53:09 +0100

    Changes in area-Z plot for the "Final report"

--- tests/area_z_relationship.py
+++ tests/area_z_relationship.py
@@ -18,10 +18,11 @@ if __name__ == "__main__":
         y = areas
         y_filtered = savgol_filter(y, window_length=51, polyorder=3)
 
-        plt.plot(x, y, label="Original values")
-        plt.plot(x, y_filtered, label="Savitzky–Golay filter")
+        plt.plot(x, y, '.', markersize=1.5, label="Valores experimentales")
+        plt.plot(x, y_filtered, label="Filtro de Savitzky–Golay")
         plt.legend()
         plt.grid()
-        plt.xlabel("Z")
-        plt.ylabel("Area in pixels")
+        plt.xlabel("Posición Z (m)")
+        plt.ylabel("Área en píxeles")
+        plt.gcf().tight_layout()
         plt.show()
